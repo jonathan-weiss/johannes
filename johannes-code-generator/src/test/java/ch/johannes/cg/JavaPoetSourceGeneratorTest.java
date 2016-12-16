3b41544feb6bd0320b81f8d0996b044c51a47751
Jonathan Weiss <jonathan.weiss@ergon.ch>
2016-12-16 15:57:08 +0100

    Add java poet as code generation library

---
+++ johannes-code-generator/src/test/java/ch/johannes/cg/JavaPoetSourceGeneratorTest.java
@@ -0,0 +1,18 @@
+package ch.johannes.cg;
+
+import org.junit.Test;
+
+import static org.hamcrest.core.Is.is;
+import static org.hamcrest.core.StringStartsWith.startsWith;
+import static org.junit.Assert.*;
+
+public class JavaPoetSourceGeneratorTest {
+
+    private static final String EXPECTED_JAVA_SOURCE = "package com.example.helloworld;";
+
+    @Test
+    public void generateCode() throws Exception {
+        JavaPoetSourceGenerator cg = new JavaPoetSourceGenerator();
+        assertThat(cg.generateCode(), startsWith(EXPECTED_JAVA_SOURCE));
+    }
+}
