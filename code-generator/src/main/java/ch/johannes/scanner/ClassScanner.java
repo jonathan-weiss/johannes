65ff6146c3d9e3ed29ed317ca9ce3392d3d401f3
Jonathan Weiss <jonathan.weiss@ergon.ch>
2016-12-19 22:36:41 +0100

    Add class scanner

---
+++ code-generator/src/main/java/ch/johannes/scanner/ClassScanner.java
@@ -0,0 +1,63 @@
+package ch.johannes.scanner;
+
+import com.google.common.base.Preconditions;
+import io.github.lukehutch.fastclasspathscanner.FastClasspathScanner;
+import io.github.lukehutch.fastclasspathscanner.scanner.ClassInfo;
+
+import java.util.Collection;
+import java.util.List;
+import java.util.Map;
+import java.util.regex.Pattern;
+import java.util.stream.Collectors;
+
+public class ClassScanner {
+
+    private final String rootPackage;
+
+    private final List<String> regexPatterns;
+
+    public ClassScanner(String rootPackage, List<String> wildcardPatterns) {
+        Preconditions.checkNotNull(wildcardPatterns, "wildcardPatterns list must not be null");
+        Preconditions.checkNotNull(rootPackage, "rootPackage must not be null");
+        //from http://stackoverflow.com/questions/19107165/search-with-wildcard-in-a-collection-of-string
+        this.regexPatterns = wildcardPatterns
+                .stream()
+                .map(s -> s.replaceAll("\\*", "\\\\w*"))
+                .collect(Collectors.toList());
+        this.rootPackage = rootPackage;
+    }
+
+    public Collection<Class<?>> scanForClasses() {
+        final Map<String, ClassInfo> allClasses = new FastClasspathScanner(rootPackage).scan().getClassNameToClassInfo();
+
+        final Collection<Class<?>> result = allClasses
+                .values()
+                .stream()
+                .filter(this::matchAnyRegexPattern)
+                .map(this::fetchClassFromClassInfo)
+                .collect(Collectors.toList());
+
+        return result;
+    }
+
+    private boolean matchAnyRegexPattern(ClassInfo classInfo) {
+        final String className = fetchClassFromClassInfo(classInfo).getSimpleName();
+        System.out.println(String.format("Classname '%s' for info %s", className, classInfo));
+
+        for(String regexPattern : this.regexPatterns) {
+            if(className.matches(regexPattern)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private Class<?> fetchClassFromClassInfo(ClassInfo classInfo) {
+        try {
+            return Class.forName(classInfo.getClassName());
+        } catch (ClassNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+}
---
+++ code-generator/src/test/java/ch/johannes/scanner/ClassScannerTest.java
@@ -0,0 +1,57 @@
+package ch.johannes.scanner;
+
+import ch.johannes.FileUtil;
+import ch.johannes.descriptor.ClassDescriptor;
+import ch.johannes.descriptor.ClassnameDescriptor;
+import ch.johannes.descriptor.FieldDescriptor;
+import ch.johannes.descriptor.PackageDescriptor;
+import ch.johannes.descriptor.TypeDescriptor;
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+import java.util.regex.Pattern;
+
+import static org.hamcrest.Matchers.contains;
+import static org.hamcrest.Matchers.containsInAnyOrder;
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.*;
+
+public class ClassScannerTest {
+
+
+    private static final String ROOT_PACKAGE = "ch.johannes";
+
+    @Test
+    public void scanForClassFileUtil() throws Exception {
+        //Arrange
+        List<String> wildcardPatternList = new ArrayList<>();
+        wildcardPatternList.add("FileUtil");
+        ClassScanner classScanner = new ClassScanner(ROOT_PACKAGE, wildcardPatternList);
+        //Act
+        final Collection<Class<?>> classes = classScanner.scanForClasses();
+
+        //Assert
+        assertThat(classes.isEmpty(), is(Boolean.FALSE));
+        assertThat(classes, contains(FileUtil.class));
+    }
+
+    @Test
+    public void scanForDescriptorClasses() throws Exception {
+        //Arrange
+        List<String> wildcardPatternList = new ArrayList<>();
+        wildcardPatternList.add("*Descriptor");
+        ClassScanner classScanner = new ClassScanner(ROOT_PACKAGE, wildcardPatternList);
+        //Act
+        final Collection<Class<?>> classes = classScanner.scanForClasses();
+
+        //Assert
+        assertThat(classes, containsInAnyOrder(
+                ClassDescriptor.class,
+                ClassnameDescriptor.class,
+                FieldDescriptor.class,
+                PackageDescriptor.class,
+                TypeDescriptor.class));
+    }
+}
