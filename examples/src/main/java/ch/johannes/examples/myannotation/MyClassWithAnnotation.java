1f02955c9b84be5f09c42626b3650bdba9dbd70c
Jonathan Weiss <jonathan.weiss@ergon.ch>
2017-02-05 14:36:00 +0100

    introduce annotation processing plugin for code generation

---
+++ examples/src/main/java/ch/johannes/examples/myannotation/MyClassWithAnnotation.java
@@ -0,0 +1,8 @@
+package ch.johannes.examples.myannotation;
+
+import ch.johannes.playground.MyDiscoveredAnnotation;
+
+@MyDiscoveredAnnotation(type=String.class, id="myId")
+public class MyClassWithAnnotation {
+
+}
