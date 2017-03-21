8da048b807effd48276e2531a7b7bd8f00e4e223
Jonathan Weiss <jonathan.weiss@ergon.ch>
2017-03-21 22:42:49 +0100

    Use single class for main and test data

--- example-data-generator/src/main/java/ch/johannes/example/generator/CodeGeneratorRunnable.java
+++ example-data-generator/src/main/java/ch/johannes/example/generator/CodeGeneratorRunnable.java
@@ -2,6 +2,7 @@ package ch.johannes.example.generator;
 
 import ch.johannes.cg.BeanSourceGenerator;
 import ch.johannes.cg.dummy.MapperSourceGenerator;
+import ch.johannes.cg.dummy.MapperTestSourceGenerator;
 import ch.johannes.core.JavaSourceWriter;
 import ch.johannes.descriptor.ClassDescriptor;
 import ch.johannes.descriptor.ClassnameDescriptor;
@@ -13,20 +14,21 @@ import ch.johannes.plan.BeanSourcePlan;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 
-public class MainCodeGeneratorRunnable {
+public class CodeGeneratorRunnable {
 
     public static void main(String[] args) {
-        System.out.println(" -!-!-!-!-! Write my main source files " + args[0]);
-        String path = args[0];
-        runCodeGeneration(Paths.get(path));
+        String pathMain = args[0];
+        String pathTest = args[1];
+        runCodeGeneration(Paths.get(pathMain), Paths.get(pathTest));
     }
 
-    public static void runCodeGeneration(Path mainSourceFilesBase) {
+    public static void runCodeGeneration(Path mainSourceFilesBase, Path testSourceFilesBase) {
         try {
             PackageDescriptor personBeanPackage = PackageDescriptor.of("ch.johannes.examples.bean");
             PackageDescriptor personTransferPackage = PackageDescriptor.of("ch.johannes.examples.transfer");
 
             JavaSourceWriter mainJavaSourceWriter = new JavaSourceWriter(mainSourceFilesBase);
+            JavaSourceWriter testJavaSourceWriter = new JavaSourceWriter(testSourceFilesBase);
             BeanSourceGenerator cg = new BeanSourceGenerator();
 
             //generate person bean descriptor
@@ -71,6 +73,20 @@ public class MainCodeGeneratorRunnable {
                     personTODescriptor.getTypeDescriptor().getClassPackage(),
                     ClassnameDescriptor.of(mapperName),
                     sourceCodeForPersonMapper);
+
+            //generate test
+            String mapperTestName = "MyGeneratedPersonMapperTest";
+            MapperTestSourceGenerator mapperTestCg = new MapperTestSourceGenerator();
+            final String sourceCodeForPersonMapperTest = mapperTestCg.generateCode(
+                    personDescriptor,
+                    personTODescriptor,
+                    personTODescriptor.getTypeDescriptor().getClassPackage(),
+                    ClassnameDescriptor.of(mapperTestName));
+            testJavaSourceWriter.writeJavaSourceFile(
+                    personTODescriptor.getTypeDescriptor().getClassPackage(),
+                    ClassnameDescriptor.of(mapperTestName),
+                    sourceCodeForPersonMapperTest);
+
 
         } catch (Exception e) {
             throw new RuntimeException(e);
